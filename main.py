85c5fcdc3d30d2c70e2b5d4f6e934fd5512e064b
Lucas Guiss <lucasguissgusmao@gmail.com>
2023-04-01 11:19:04 -0300

    refactor(routes): remove unecessary apirouter

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI, Request
-from app.routers import shortener
-from app import database
+from fastapi import FastAPI, Request, Body, Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from app import database
+from app.models import ShortenerCreate
+from app.database import get_db
+from app.shortener import create_shortened_url, redirect_shortened
+
 
 templates = Jinja2Templates(directory="templates")
 
@@ -12,8 +15,14 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-app.include_router(shortener)
-
 @app.get("/")
 def root(request: Request):
     return templates.TemplateResponse("create.html", {"request": request })
+
+@app.post("/")
+def create(payload: ShortenerCreate = Body(...), db = Depends(get_db)):
+    return create_shortened_url(payload=payload, db=db)
+
+@app.get("/{shortener_id}")
+def redirect(request: Request, shortener_id: str, db = Depends(get_db)):
+    return redirect_shortened(db, request, shortener_id)
