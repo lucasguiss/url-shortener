0af3722fb895e8e5add84d6c8f201fa41c1c3cf7
Lucas Guiss <lucasguissgusmao@gmail.com>
2023-04-01 01:20:31 -0300

    feat(create): add simple page to create shortened url

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.routers import shortener
 from app import database
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates")
 
 database.Base.metadata.create_all(bind=database.engine)
 
@@ -12,5 +15,5 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(shortener)
 
 @app.get("/")
-def root():
-    return { "message": "running" }
+def root(request: Request):
+    return templates.TemplateResponse("create.html", {"request": request })
